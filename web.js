daf28968c41c543643e05974c3c93ad6a8704324
Kevin Tomasso <ktomasso@gmail.com>
2013-07-21 23:29:13 +0000

    Attempt #1 for passing index.html into heroku site

--- web.js
+++ web.js
@@ -2,9 +2,14 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+var fs = require('fs');
+var file = readFileSync(index.html);
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send('file');
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
